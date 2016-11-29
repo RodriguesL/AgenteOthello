af9d5a7d017c80abffb833f60fc88debc26c1a0f
Lucas Rodrigues <rodriguesl@dcc.ufrj.br>
2016-11-29 18:05:47 -0200

    Correções na implementação gulosa, mas ainda falta coisa

--- Tabuleiro/models/players/guloso_player.py
+++ Tabuleiro/models/players/guloso_player.py
@@ -10,16 +10,23 @@ class Guloso:
         melhorMovimento = None
         melhorPontuacao = -1
         for move in moves:
-            x,y = move
-            [white, black] = board.score()
-            #Não tá certo, mas a lógica é essa. Precisa achar um jeito de "simular" move(x,y) sem de fato fazer a jogada e analisar as pontuações de todos os moves possíveis
-            if self.color == board.WHITE:
-                pontuacao = white
+            simulacao_jogada = board.get_clone()
+            simulacao_jogada.play(self, move, color)
+            [white, black] = simulacao_jogada.score()
+            if color == board.WHITE:
+                pontuacao_temp = white
             else:
-                pontuacao = black
+                pontuacao_temp = black
 
-            if pontuacao > melhorPontuacao:
-                melhorPontuacao = pontuacao
+            if pontuacao_temp > melhorPontuacao:
+                melhorPontuacao = pontuacao_temp
+                melhorMovimento = move
+
+        return melhorMovimento
+
+
+
+
 
 
 
